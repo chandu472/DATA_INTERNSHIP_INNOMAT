922ec9a915a48584a38eae1f97dcea5bfdf8fc35
chandu472 <77053801+chandu472@users.noreply.github.com>
2023-02-26 19:34:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,38 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-st.header("Hello World")
-st.header("hi")
+# Load the dataset
+data = pd.read_csv('iris_.csv')
+
+# Sidebar options
+st.sidebar.header('Options')
+plot_type = st.sidebar.selectbox('Select a plot type', ('Histogram', 'Boxplot', 'Scatterplot'))
+
+# Plotting
+st.header('Data Visualization Dashboard')
+
+if plot_type == 'Histogram':
+    feature = st.selectbox('Select a feature', data.columns)
+    fig, ax = plt.subplots()
+    ax.hist(data[feature])
+    st.pyplot(fig)
+
+elif plot_type == 'Boxplot':
+    feature = st.selectbox('Select a feature', data.columns)
+    fig, ax = plt.subplots()
+    ax.boxplot(data[feature])
+    st.pyplot(fig)
+
+else:
+    x_feature = st.selectbox('Select a feature for x-axis', data.columns)
+    y_feature = st.selectbox('Select a feature for y-axis', data.columns)
+    fig, ax = plt.subplots()
+    ax.scatter(data[x_feature], data[y_feature])
+    st.pyplot(fig)
+
+# Data table
+st.header('Data Table')
+st.write(data)
